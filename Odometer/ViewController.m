3aa5e7de2b03c706b0bdc07d739cc0f17cf4ba55
Jay Versluis <jay@versluis.com>
2015-11-30 18:16:34 -0500

    works in principle

--- Odometer/ViewController.m
+++ Odometer/ViewController.m
@@ -9,9 +9,11 @@
 #import "ViewController.h"
 @import CoreLocation;
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
 
 @property (strong, nonatomic) IBOutlet UILabel *textlabel;
+@property (strong, nonatomic) CLLocationManager *manager;
+@property (strong, nonatomic) CLLocation *previousLocation;
 
 @end
 
@@ -20,11 +22,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.textlabel.text = nil;
+    
+    // beg for location usage
+    [self.manager requestAlwaysAuthorization];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (CLLocationManager *)manager {
+    
+    if (!_manager) {
+        _manager = [[CLLocationManager alloc]init];
+        _manager.delegate = self;
+    }
+    return _manager;
 }
 
 - (IBAction)startStopButtonPressed:(id)sender {
@@ -44,11 +58,62 @@
 - (void)startOdometer {
     
     self.textlabel.text = @"Measuring...";
+    
+    // start location manager
+    [self.manager startUpdatingLocation];
+    
 }
 
 - (void)stopOdometer {
     
     self.textlabel.text = @"Stopped";
+    
+    // stop location manager
+    [self.manager stopUpdatingLocation];
+}
+
+#pragma mark - Location Manager Delegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    
+    NSLog(@"Location Update received...");
+    [self calculateDistanceForLocation:locations.lastObject];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    
+    NSLog(@"Nothing works - like always!");
+    NSLog(@"Description: %@\nReason: %@", error.localizedDescription, error.localizedFailureReason);
+}
+
+- (void)calculateDistanceForLocation:(CLLocation *)currentLocation {
+    
+    // do we have a previous location?
+    if (!self.previousLocation) {
+        self.previousLocation = currentLocation;
+        return;
+    }
+    
+    // how much time has passed between loation updates?
+    // NSTimeInterval duration = [self.previousLocation.timestamp timeIntervalSinceDate:currentLocation.timestamp];
+    
+    // what's the distance between locations (in meters)?
+    CLLocationDistance distance = [self.previousLocation distanceFromLocation:currentLocation];
+    
+    // turn it into kilometers per hour
+    distance = distance * 3.6;
+    
+    // update label
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc]init];
+    formatter.maximumFractionDigits = 0;
+    formatter.minimumIntegerDigits = 1;
+    
+    NSString *distanceText = [NSString stringWithFormat:@"%@ k/ph", [formatter stringFromNumber:[NSNumber numberWithFloat:distance]]];
+    self.textlabel.text = distanceText;
+    
+    // store current location as previous
+    self.previousLocation = currentLocation;
+    
 }
 
 @end
