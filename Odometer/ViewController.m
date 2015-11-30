79bf334e7e0d523c4857251e5b1799c3f0c2825e
Jay Versluis <jay@versluis.com>
2015-11-30 17:12:28 -0500

    added label and start/stop button

--- Odometer/ViewController.m
+++ Odometer/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+@import CoreLocation;
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UILabel *textlabel;
 
 @end
 
@@ -16,12 +19,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.textlabel.text = nil;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)startStopButtonPressed:(id)sender {
+    
+    // let's toggle this guy
+    if ([self.navigationItem.rightBarButtonItem.title isEqualToString:@"Start"]) {
+        self.navigationItem.rightBarButtonItem.title = @"Stop";
+        // start Odometer
+        [self startOdometer];
+    } else {
+        self.navigationItem.rightBarButtonItem.title = @"Start";
+        // stop Odometer
+        [self stopOdometer];
+    }
+}
+
+- (void)startOdometer {
+    
+    self.textlabel.text = @"Measuring...";
+}
+
+- (void)stopOdometer {
+    
+    self.textlabel.text = @"Stopped";
 }
 
 @end
