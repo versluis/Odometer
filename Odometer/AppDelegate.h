0ebdba497ffbe3f1698856558fd92c3d35a7b2f2
Jay Versluis <jay@versluis.com>
2015-11-30 17:03:45 -0500

    Initial Commit

---
+++ Odometer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Odometer
+//
+//  Created by Jay Versluis on 30/11/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Odometer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Odometer
+//
+//  Created by Jay Versluis on 30/11/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
