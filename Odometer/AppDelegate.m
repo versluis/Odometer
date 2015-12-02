88a39440786b0295dde77d9284646d2f89b6b43d
Jay Versluis <jay@versluis.com>
2015-12-01 19:02:38 -0500

    the total distance is now saved to user defaults and will survive app restarts

--- Odometer/AppDelegate.m
+++ Odometer/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,12 +18,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [self retrieveTotalDistance];
+    
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    [self storeTotalDistance];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -40,6 +46,31 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    [self storeTotalDistance];
+}
+
+- (void)storeTotalDistance {
+    
+    // save total distance in user defaults
+    UINavigationController *nav = (UINavigationController *)self.window.rootViewController;
+    ViewController *viewController = (ViewController *)nav.viewControllers.lastObject;
+    NSNumber *totalDistance = [NSNumber numberWithFloat:viewController.totalDistance];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:totalDistance forKey:@"totalDistance"];
+    [defaults synchronize];
+}
+
+- (void)retrieveTotalDistance {
+    
+    // retrieve total distance from user defaults
+    UINavigationController *nav = (UINavigationController *)self.window.rootViewController;
+    ViewController *viewController = (ViewController *)nav.viewControllers.lastObject;
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSNumber *totalDistance = [defaults objectForKey:@"totalDistance"];
+    viewController.totalDistance = [totalDistance floatValue];
 }
 
 @end
